1b8ce74238333d80f6ed838e00a6ac23f4ba7d16
Dafe-akaka <dafe.akaka123@gmail.com>
2021-12-11 20:05:14 +0000

    update

---
+++ src/components/TransactionList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  //console.log(context) - gives transactions array
+  //instead of doing context.transactions, can destrcuture and pull out transactions array, which needs to be mapped through
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction, key) => (
+          <Transaction transaction={transaction} key={transaction.id}/>))}
+      </ul>
+    </>
+  );
+};
